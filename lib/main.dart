6296536c8571bd5ae28dbd0d79ac9dc10b4a1387
pbellwon <pawelbellwon@gmail.com>
2026-08-04 18:17:31 +0200

    a1 - adding anonymous auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -15,8 +15,13 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  if (FirebaseAuth.instance.currentUser == null) {
+    await FirebaseAuth.instance.signInAnonymously();
+  }
+
   runApp(const FreeWaterApp());
 }
+
 
 class FreeWaterApp extends StatelessWidget {
   const FreeWaterApp({super.key});
